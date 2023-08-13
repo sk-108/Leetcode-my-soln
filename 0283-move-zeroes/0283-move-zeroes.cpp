838f24d54e962c9a789d2eb06ca5bbcdf1ea8bcd
Sourav Kumar <77422786+sk-108@users.noreply.github.com>
2023-08-13 16:11:03 +0530

    Time: 21 ms (69.62%), Space: 19.2 MB (31.20%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    void rec(vector<int>&arr,int i,int size)
-    {
-        if(i == size)
-        {
-            return ;
-        }
-        else{
-            if(arr[i] != 0)
-            {
-                return rec(arr,i+1,size);
-            }
-            else{
-                rec(arr,i+1,size);
-                int temp = arr[i];
-                for(int c=i;c<size-1;c++)
-                {
-                    arr[c] = arr[c+1];
-                }
-                arr[size-1] = 0;
-            }
-        }
-        return ;
-    }
     void moveZeroes(vector<int>& nums) {
-        int zeroptr = 0;
-        int itr = 0;
-        rec(nums,0,nums.size());
-        return ;
+        int l = 0;
+        int r = 0;
+        while(r<nums.size())
+        {
+            if(nums[r] != 0)
+            {
+                swap(nums[r],nums[l]);
+                l++;
+            }
+            r++;
+        }
+        
     }
 };
